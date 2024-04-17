fef77db635cb185b5c47159fa6e775016963a123
Rodrigo Avila <rodrigo.avilaca@usm.cl>
2024-04-17 15:17:38 -0400

    tb bien

--- sesion 3/tbdeco2.sv
+++ sesion 3/tbdeco2.sv
@@ -1,13 +1,18 @@
 module tbdeco();
-    logic A;
+    logic [1:0] A;
     logic [3:0] one_hot;
 
     deco_N2 DUT(
         .A(A),
-        .B(B),
         .one_hot(one_hot)
     );
     initial begin
- 
+        A = 2'd0;
+        #2
+        A = 2'd1;
+        #2
+        A = 2'd2;
+        #2
+        A = 2'd3;
     end
 endmodule
